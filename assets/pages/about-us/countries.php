e7b6474b0aae607645f778265dd6236e52d588ee
augustin-55 <40851893+augustin-55@users.noreply.github.com>
2021-01-27 22:36:29 +0100

    27-01-2021-V1.08

--- assets/pages/about-us/countries.php
+++ assets/pages/about-us/countries.php
@@ -1,50 +1,51 @@
-<h4 class="text-center">~ <i class="fas fa-flag"></i> About our countries ~</h4>
-
-<div class="flex-row">
-        <div class="flex-home">
-            <!-- Welcome ! -->
-            
-            <!-- Main : Philippines -->
-            <section class="about-home-philippines">
-                <h2>About Philippines</h2>
-                <div class="flex">
-                    <div class="map-about">
-                        <img src="assets/pictures/home/philippines-map" alt="map of Philippines">
-                    </div>
-                    <article>
-                        <div class="description-about">
-                                <img src="assets/pictures/home/france-flag.png" alt="Flag of France">
-                                <p>The Philippines, in long form the Republic of the Philippines (in Filipino : Pilipinas or Republika ng Pilipinas), is a country in Southeast Asia made up of an archipelago of 7,641 islands of which eleven total more than 90% of the land and of which just over 2,000 are inhabited, while about 2,400 islands did not even receive a name.</p>
-                                <p>There are three geographic areas : Luçon, Visayas and Mindanao. Luzon is the largest and most northerly island, home to its capital, Manila, and the country's largest city, Quezon City.</p>
-                                <p>Manila, is famous for its oceanfront walks and its centuries-old Chinatown, Binondo. Intramuros, the heart of the old town, was a fortified town during the colonial era. This neighborhood is home to the 17th-century Baroque Church of St. Augustin and Fort Santiago, a multi-story military prison and citadel.</p>
-                            </div>
-                            <div>
-                                <p class="text-center"><a href="#">Wanna see more ?</a></p>
-                            </div>
-                        </div>  
-                   </article>
+<div class="main-content-countries">
+    <h4 class="text-center">~ <i class="fas fa-flag"></i> About our countries ~</h4>
+    <div class="flex-row">
+        <!-- Main : Philippines -->
+        <section class="about-philippines">
+            <div class="flex">
+                <div class="map-about">
+                    <img src="assets/pictures/home/philippines-map" alt="map of Philippines">
                 </div>
-            </section>
-            <!-- Main : France -->
-            <section class="about-home-france">
-                <h2>About France</h2>
-                <div class="flex">
-                    <div class="map-about">
-                        <img src="assets/pictures/home/france-map" alt="map of France">
+                <article>
+                    <div class="description-about">
+                        <img src="assets/pictures/home/philippines-flag.png" alt="Flag of France">
+                        <p>Philippines, in long form the Republic of the Philippines (in Filipino : Pilipinas or Republika ng Pilipinas), is a country in Southeast Asia made up of an archipelago of 7,641 islands of which eleven total more than 90% of the land and of which just over 2,000 are inhabited, while about 2,400 islands did not even receive a name.</p>
+                        <p>There are three geographic areas : Luçon, Visayas and Mindanao. Luzon is the largest and most northerly island, home to its capital, Manila, and the country's largest city, Quezon City.</p>
+                        <p>Manila, is famous for its oceanfront walks and its centuries-old Chinatown, Binondo. Intramuros, the heart of the old town, was a fortified town during the colonial era. This neighborhood is home to the 17th-century Baroque Church of St. Augustin and Fort Santiago, a multi-story military prison and citadel.</p>
                     </div>
-                    <article>
-                        <div class="description-about">
-                                <img src="assets/pictures/home/france-flag.png" alt="Flag of France">
-                                <p>The Philippines, in long form the Republic of the Philippines (in Filipino : Pilipinas or Republika ng Pilipinas), is a country in Southeast Asia made up of an archipelago of 7,641 islands of which eleven total more than 90% of the land and of which just over 2,000 are inhabited, while about 2,400 islands did not even receive a name.</p>
-                                <p>There are three geographic areas : Luçon, Visayas and Mindanao. Luzon is the largest and most northerly island, home to its capital, Manila, and the country's largest city, Quezon City.</p>
-                                <p>Manila, is famous for its oceanfront walks and its centuries-old Chinatown, Binondo. Intramuros, the heart of the old town, was a fortified town during the colonial era. This neighborhood is home to the 17th-century Baroque Church of St. Augustin and Fort Santiago, a multi-story military prison and citadel.</p>
-                            </div>
-                            <div>
-                                <p class="text-center"><a href="#">Wanna see more ?</a></p>
-                            </div>
-                        </div>  
-                   </article>
+                    <div>
+                        <p class="text-center"><a href="#">Wanna see more ?</a></p>
+                    </div>
+                    <div>
+                        <hr>
+                        <h4><button>What do we love in Philippines ?</button></h4>
+                    </div>
+                </article>
+            </div>
+        </section>
+        <!-- Main : France -->
+        <section class="about-france">
+            <div class="flex">
+                <div class="map-about">
+                    <img src="assets/pictures/home/france-map" alt="map of France">
                 </div>
-            </section>
-        </div>
+                <article>
+                    <div class="description-about">
+                        <img src="assets/pictures/home/france-flag.png" alt="Flag of France">
+                        <p>The Philippines, in long form the Republic of the Philippines (in Filipino : Pilipinas or Republika ng Pilipinas), is a country in Southeast Asia made up of an archipelago of 7,641 islands of which eleven total more than 90% of the land and of which just over 2,000 are inhabited, while about 2,400 islands did not even receive a name.</p>
+                        <p>There are three geographic areas : Luçon, Visayas and Mindanao. Luzon is the largest and most northerly island, home to its capital, Manila, and the country's largest city, Quezon City.</p>
+                        <p>Manila, is famous for its oceanfront walks and its centuries-old Chinatown, Binondo. Intramuros, the heart of the old town, was a fortified town during the colonial era. This neighborhood is home to the 17th-century Baroque Church of St. Augustin and Fort Santiago, a multi-story military prison and citadel.</p>
+                    </div>  
+                    <div>
+                        <p class="text-center"><a href="#">Wanna see more ?</a></p>
+                    </div>
+                    <div>
+                        <hr>
+                        <h4><button>What do we love in France ?</button></h4>
+                    </div>
+                </article>
+            </div>
+        </section>
     </div>
+</div>
